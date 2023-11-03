6232ad05ac97d992f09671e03beee019893976d0
Sreejith36 <sreejith8230@gmail.com>
2023-11-03 14:53:52 +0530

    card flip shuffle done

--- app1.js
+++ app1.js
@@ -1,9 +1,19 @@
 const mycards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+
+    if(lockBoard === true){
+        return;
+    }
+
+    if(this === firstCard){
+        return;
+    }
+
     this.classList.toggle('flip');
     this.classList.add('flip');
 
@@ -14,7 +24,7 @@ function flipCard(){
     }
 
     secondCard = this;
-    hasFlippedCard = false;
+    // hasFlippedCard = false;
 
     checkForMatch();
 
@@ -32,15 +42,33 @@ function checkForMatch(){
 function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 
 function unflipCards(){
+    lockBoard = true;
     setTimeout(
         () => {
             firstCard.classList.remove('flip');
             secondCard.classList.remove('flip');
-        }, 1500
-    )
+            // lockBoard = false;
+            resetBoard();
+        }, 1500 )
 }
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+mycards.forEach( (card) => {
+    
+    let randomNum = Math.floor(Math.random() * 12);
+    card.style.order = randomNum;
+
+});
+})()
+
 
 mycards.forEach(card => card.addEventListener('click', flipCard));

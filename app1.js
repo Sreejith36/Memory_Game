b62f0626c4fe92a6ba2e4b4a7bd2ec9952103862
Sreejith36 <sreejith8230@gmail.com>
2023-11-03 13:27:16 +0530

    checking the two clicked card matching or not

--- app1.js
+++ app1.js
@@ -1,7 +1,46 @@
 const mycards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard(){
     this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(hasFlippedCard === false){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+
+}
+
+function checkForMatch(){
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+    setTimeout(
+        () => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+        }, 1500
+    )
 }
 
 mycards.forEach(card => card.addEventListener('click', flipCard));
